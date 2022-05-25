5ad3e0a512bd332d57fc7f0c6874c5ee11136e87
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-26 03:39:44 +0600

    Delete app test files

--- estatejet/apps/user/model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import String, Integer
-from sqlalchemy import Column
-
-from estatejet.db import Base
-
-
-class UserItem(Base):
-    name = Column(String)
-    description = Column(String)
--- estatejet/models/item.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import String, Integer
-from sqlalchemy import Column
-
-from estatejet.db import Base
-
-
-class Item(Base):
-    name = Column(String)
-    description = Column(String)
