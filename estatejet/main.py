251133b6b5890ea3551eb0f2d6925dbb1aa33646
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-27 02:27:52 +0600

    :sparkles: Testing User Model API

--- estatejet/main.py
+++ estatejet/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
-from estatejet.db import Base, engine
+from estatejet.apps.user.models import User
+from estatejet.apps.user.schema import UserCreateModel
+from estatejet.db import Base, engine, startup
 
 app = FastAPI()
 
@@ -9,9 +11,28 @@ Base.metadata.create_all(bind=engine)
 App = app
 
 
+@app.on_event("startup")
+async def on_startup():
+    await startup()
+
+
 @app.get("/")
 async def root():
     return {"message": "Welcome To EstateJet"}
+
+
+@app.get("/create")
+def create():
+    user = UserCreateModel(email="email@gmai2l.com",
+                           first_name="First Name",
+                           last_name="Last Name",
+                           country_code="+123",
+                           phone_number="123456567",
+                           role=1,
+                           password="Password")
+
+    data = User.create(user)
+    return {"message": "Success"}
 
 
 if __name__ == "__main__":
