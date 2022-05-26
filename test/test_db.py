7e8105c91970dec52d9af16084cef59656b98a54
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-27 02:29:23 +0600

    :rotating_light: User Creation Test

---
+++ test/test_db.py
@@ -0,0 +1,19 @@
+from estatejet.apps.user.models import User
+from estatejet.apps.user.schema import UserCreateModel
+from test.utils.db import TestDatabase
+
+
+class TestUserModel(TestDatabase):
+
+    def test_user_create(self):
+        user = UserCreateModel(email="email@gmail.com",
+                               first_name="First Name",
+                               last_name="Last Name",
+                               country_code="+12",
+                               phone_number="123456",
+                               role=1,
+                               password="Password")
+
+        self.assertEqual(user.email, "email@gmail.com")
+        data = User.create(self.session, user)
+        self.assertEqual(data.email, "email@gmail.com")
