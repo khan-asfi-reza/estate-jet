bad7042143e1b2db4595517917897955cff77efc
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-28 03:17:37 +0600

    :hammer: Database Config File Refactoring

--- estatejet/db/db.py
+++ estatejet/db/db.py
@@ -2,19 +2,34 @@ import typing as t
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, DateTime, func, Integer
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
-from sqlalchemy.orm import sessionmaker, Query
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-
 from estatejet.config.settings import Config
 
-engine = create_engine(
-    Config.DATABASE_URL
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class DatabaseSessionLayer:
+    connection = None
+    engine = None
+    conn_string = None
+    session = None
+
+    def __init__(self, database_url=None):
+        if database_url is None:
+            database_url = Config.PYTEST_DATABASE_URL
+
+        self.engine = create_engine(database_url)
+
+    def create_session(self):
+        Base.metadata.create_all(self.engine)
+        self.connection = self.engine.connect()
+        self.session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        return self.session()
+
 
 class_registry: t.Dict = {}
 
 
+# Base Class for models and database registry
 @as_declarative(class_registry=class_registry)
 class Base:
     __name__: str
@@ -29,64 +44,22 @@ class Base:
         return self.__name__.lower()
 
 
+# Base Pydantic Model
 class BasePyModel(BaseModel):
     id: int
     created_on: datetime
     updated_on: datetime
 
 
-class TestDatabase:
-    connection = None
-    engine = None
-    conn_string = None
-    session = None
-
-    def db_init(self):
-        pass
-
-    def create_session(self):
-        self.engine = create_engine(Config.TEST_DATABASE_URL)
-        Base.metadata.create_all(self.engine)
-        self.connection = self.engine.connect()
-        self.session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-        return self.session()
+# Global Session, Engine
+DatabaseLayer = DatabaseSessionLayer()
+Session = DatabaseLayer.create_session()
+engine = DatabaseLayer.engine
 
 
-TestDatabaseLayer = TestDatabase()
-
-Session = TestDatabaseLayer.create_session()
-
-
+# On App Startup Run This Function
 async def startup():
-    global Session
-    Session = SessionLocal()
-
-
-class Model(Base):
-    __abstract__ = True
-
-    @classmethod
-    def create(cls, data: BaseModel):
-        db_object = cls(**data.dict())
-        Session.add(db_object)
-        Session.commit()
-        Session.refresh(db_object)
-        return db_object
-
-    @staticmethod
-    def update(query: Query, **kwargs):
-        for field in kwargs:
-            setattr(query, field, kwargs[field])
-        Session.commit()
-
-    @classmethod
-    def get(cls, index: int) -> Query:
-        return Session.query(cls).filter(cls.id == index).first()
-
-    @classmethod
-    def filter(cls, **kwargs) -> Query:
-        filter_set = []
-        for args in kwargs:
-            filter_set.append(getattr(cls, args) == kwargs[args])
-
-        return Session.query(cls).filter(*filter_set)
+    global Session, DatabaseLayer, engine
+    DatabaseLayer = DatabaseSessionLayer(Config.DATABASE_URL)
+    Session = DatabaseLayer.create_session()
+    engine = DatabaseLayer.engine
