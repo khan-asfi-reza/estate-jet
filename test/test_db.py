344f78d5ba5583f992db705bde3afd0575fcfe1c
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-29 02:32:48 +0600

    :rotating_light: Test database connection

--- test/test_db.py
+++ test/test_db.py
@@ -6,14 +6,5 @@ from test.utils.db import TestDatabase
 class TestUserModel(TestDatabase):
 
     def test_user_create(self):
-        user = UserCreateModel(email="email@gmail.com",
-                               first_name="First Name",
-                               last_name="Last Name",
-                               country_code="+12",
-                               phone_number="123456",
-                               role=1,
-                               password="Password")
-
-        self.assertEqual(user.email, "email@gmail.com")
-        data = User.create(user)
-        self.assertEqual(data.email, "email@gmail.com")
+        data = self.connection.execute("SELECT 1")
+        self.assertIsNotNone(data)
