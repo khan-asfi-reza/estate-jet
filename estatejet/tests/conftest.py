b1a1c7d5706e6fedcaa604dd6155b841cf18b7fc
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-08-07 14:20:12 +0600

    :white_check_mark: ConfTest for pytest

---
+++ estatejet/tests/conftest.py
@@ -0,0 +1,36 @@
+import asyncio
+import os
+import pytest
+from starlette.testclient import TestClient
+from tortoise import Tortoise
+from tortoise.contrib.test import finalizer, initializer
+from tortoise.exceptions import DBConnectionError, OperationalError
+
+from config import settings
+from db import get_tortoise_models
+from main import app
+
+
+@pytest.fixture(scope="session", autouse=True)
+def initialize_tests(request):
+    db_url = "sqlite://memory"
+
+    async def _init_db() -> None:
+        await Tortoise.init(db_url=db_url, modules={"modules": get_tortoise_models()})
+        try:
+            await Tortoise._drop_databases()
+        except (DBConnectionError, OperationalError):  # pragma: nocoverage
+            pass
+
+        await Tortoise.init(db_url=db_url, modules={"modules": get_tortoise_models()}, _create_db=True)
+        await Tortoise.generate_schemas(safe=False)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(_init_db())
+
+    request.addfinalizer(lambda: loop.run_until_complete(Tortoise._drop_databases()))
+
+
+@pytest.fixture()
+def client() -> TestClient:
+    return TestClient(app)
