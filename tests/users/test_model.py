7ce28a7c4b6a9fb326501ed849d655a940ced867
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-08-20 19:55:52 +0600

    User Model Tests

--- tests/users/test_model.py
+++ tests/users/test_model.py
@@ -1,8 +1,27 @@
+import pytest
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from estatejet.apps.users.models import Users
+from tests.fixtures.factories.user_factory import UserFactory
 
 
-def test_create_user(client: TestClient):
-    response = client.post(
+@pytest.mark.anyio
+async def test_user_factory_model_batch():
+    await UserFactory.create_batch(10)
+    models = await Users.all()
+    assert len(models) == 10
+
+
+@pytest.mark.anyio
+async def test_user_factory(users):
+    models = await Users.all()
+    assert len(models) == 1
+
+
+@pytest.mark.anyio
+async def test_create_user(client: AsyncClient):
+    response = await client.post(
         "/users/",
         json={
             "email": "admin@gmail.com",
