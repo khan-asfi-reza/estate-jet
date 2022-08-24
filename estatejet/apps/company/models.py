a14871b75086fa9f49f7dbfe3f0db15daecedf86
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-08-24 21:51:27 +0600

    Company models

--- estatejet/apps/company/models.py
+++ estatejet/apps/company/models.py
@@ -10,14 +10,15 @@ class Company(AbstractModel):
     Real Estate Company
     """
     name = fields.CharField(max_length=256)
-    admin = fields.OneToOneField('model.Users', on_delete=fields.SET_NULL, null=True)
     license_number = fields.CharField(max_length=512)
+    number_of_employees = fields.IntField(default=0)
 
 
 class RoleEnum(str, Enum):
-    TEAM_LEADER = "TEAM_LEADER"
-    MANAGER = "MANAGER"
     OWNER = "OWNER"
+    ADMIN = "ADMIN"
+    MANAGER = "MANAGER"
+    TEAM_LEADER = "TEAM_LEADER"
     AGENT = "AGENT"
 
 
@@ -25,6 +26,15 @@ class CompanyEmployee(AbstractModel):
     """
     Company User Relation Mapping
     """
-    user = fields.OneToOneField('model.Users', on_delete=fields.CASCADE)
-    company = fields.ForeignKeyField('model.Company', on_delete=fields.CASCADE)
+    user = fields.OneToOneField('users.Users', on_delete=fields.CASCADE)
+    company = fields.ForeignKeyField('company.Company', on_delete=fields.CASCADE)
     role = fields.CharEnumField(enum_type=RoleEnum, default=RoleEnum.AGENT)
+
+    def is_admin(self) -> bool:
+        """
+
+        Returns: bool, checks if user is admin
+
+        """
+        return self.role == RoleEnum.ADMIN
+
